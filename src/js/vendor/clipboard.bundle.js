53429ab22799ef6a8ae40f0bcfc5d7f01a2f42ce
Alex Mansell <alex.mansell@magnolia-cms.com>
2023-08-18 08:34:10 -0500

    code block copy updates2

--- src/js/vendor/clipboard.bundle.js
+++ src/js/vendor/clipboard.bundle.js
@@ -6,7 +6,7 @@
   for (var i = 0; i < pre.length; i++) {
     var b = document.createElement('button')
     b.className = 'clipboard'
-    b.textContent = 'Copy'
+    b.innerHTML = 'Copy <i class="fa fa-copy"></i>' // Use the Font Awesome copy icon
     if (pre[i].childNodes.length === 1 && pre[i].childNodes[0].nodeType === 3) {
       var div = document.createElement('div')
       div.textContent = pre[i].textContent
@@ -25,9 +25,16 @@
     },
   }).on('success', function (e) {
     e.clearSelection()
-    e.trigger.textContent = 'Copied'
+    var icon = e.trigger.querySelector('i')
+    icon.className = 'fa fa-check' // Change the icon to the Font Awesome check icon
+    e.trigger.removeChild(icon)
+    e.trigger.textContent = '' // Add a space before "Copied" to adjust alignment
+    e.trigger.appendChild(icon)
     setTimeout(function () {
-      e.trigger.textContent = 'Copy'
+      icon.className = 'fa fa-copy' // Revert back to the copy icon
+      e.trigger.removeChild(icon)
+      e.trigger.textContent = 'Copy ' // Revert back to "Copy" text
+      e.trigger.appendChild(icon)
     }, 2000)
   })
 })()
